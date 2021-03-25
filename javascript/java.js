ec3a0b99ae0b79a80face290692841c28caa9d26
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-03-25 15:35:29 +0200

    java edit

---
+++ javascript/java.js
@@ -0,0 +1,24 @@
+alert("email is (mohammad)");
+
+var email = prompt('enter your email');
+alert("age is (25)");
+var age = prompt('enter your age');
+if (email == "mohammad") {
+    document.write("your email is correct");
+}
+else {
+
+    document.write("your email is wrong");
+}
+if (age == "25") {
+    document.write("your age is correct");
+}
+else {
+
+    document.write("your age is wrong");
+}
+
+
+document.write('<h3>'+email+'</h3>');
+ document.write('<h3>'+age+'</h3>');
+ 
